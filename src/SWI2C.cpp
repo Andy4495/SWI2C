a890b5f8ca01117fe63678d7c32fdeec608b3ed5
Andreas Taylor <Andy4495@outlook.com>
2018-03-17 14:36:38 -0500

    Updated after testing 1-byte reads.

--- src/SWI2C.cpp
+++ src/SWI2C.cpp
@@ -1,82 +1,97 @@
 #include "SWI2C.h"
 
-void sclHi() {
-  pinMode(SCL_PIN, INPUT);         // Pull-up resistor pulls SCL high in Hi-Z mode
+SWI2C::SWI2C(uint8_t sda_pin, uint8_t scl_pin, uint8_t deviceID) {
+  _sda_pin = sda_pin;
+  _scl_pin = scl_pin;
+  _deviceID = deviceID;
+}
+
+void SWI2C::begin() {
+  // Set I2C pins low in setup. With pinMode(OUTPUT), line goes low
+  //   With pinMode(INPUT), then pull-up resistor pulls line high
+  digitalWrite(_scl_pin, LOW);
+  digitalWrite(_sda_pin, LOW);
+  pinMode(_scl_pin, INPUT);
+  pinMode(_sda_pin, INPUT);
+}
+
+void SWI2C::sclHi() {
+  pinMode(_scl_pin, INPUT);         // Pull-up resistor pulls SCL high in Hi-Z mode
   // Check to make sure SCL pin has actually gone high before returning
   // Slave may be pulling SCL low to delay transfer (clock stretching)
-  while (digitalRead(SCL_PIN) == LOW);
+  while (digitalRead(_scl_pin) == LOW);
 }
 
-void sclLo() {
-  pinMode(SCL_PIN, OUTPUT);        // SCL_PIN set LOW in constructor
+void SWI2C::sclLo() {
+  pinMode(_scl_pin, OUTPUT);        // _scl_pin set LOW in constructor
 }
 
-void sdaHi() {
-  pinMode(SDA_PIN, INPUT);
+void SWI2C::sdaHi() {
+  pinMode(_sda_pin, INPUT);
 }
 
-void sdaLo() {
-  pinMode(SDA_PIN, OUTPUT);        // SDA_PIN set LOW in constructor
+void SWI2C::sdaLo() {
+  pinMode(_sda_pin, OUTPUT);        // _sda_pin set LOW in constructor
 }
 
-void startBit() {  // Assume SDA already HIGH
+void SWI2C::startBit() {  // Assume SDA already HIGH
   sclHi();
   sdaLo();
   sclLo();
 }
 
-void writeAddress(int address, int r_w) {  // Assume SCL, SDA already LOW from startBit()
-  if (address & 0x40) sdaHi();     // bit 6
+void SWI2C::writeAddress(int r_w) {  // Assume SCL, SDA already LOW from startBit()
+  if (_deviceID & 0x40) sdaHi();     // bit 6
   else sdaLo();
   sclHi();
   sclLo();
-  if (address & 0x20) sdaHi();     // bit 5
+  if (_deviceID & 0x20) sdaHi();     // bit 5
   else sdaLo();
   sclHi();
   sclLo();
-  if (address & 0x10) sdaHi();     // bit 4
+  if (_deviceID & 0x10) sdaHi();     // bit 4
   else sdaLo();
   sclHi();
   sclLo();
-  if (address & 0x08) sdaHi();     // bit 3
+  if (_deviceID & 0x08) sdaHi();     // bit 3
   else sdaLo();
   sclHi();
   sclLo();
-  if (address & 0x04) sdaHi();     // bit 2
+  if (_deviceID & 0x04) sdaHi();     // bit 2
   else sdaLo();
   sclHi();
   sclLo();
-  if (address & 0x02) sdaHi();     // bit 1
+  if (_deviceID & 0x02) sdaHi();     // bit 1
   else sdaLo();
   sclHi();
   sclLo();
-  if (address & 0x01) sdaHi();     // bit 0
+  if (_deviceID & 0x01) sdaHi();     // bit 0
   else sdaLo();
   sclHi();
   sclLo();
   if (r_w == 1) sdaHi();         // R/W bit
   sclHi();
   sclLo();
-  sdaHi();    // Release the bus for ACK signal from slave
+  sdaHi();    // Release the data line for ACK signal from slave
 }
 
-uint8_t checkAckBit() { // Can also be used by master to send NACK after last byte is read from slave
+uint8_t SWI2C::checkAckBit() { // Can also be used by master to send NACK after last byte is read from slave
   uint8_t ack;
-  sdaHi();    // Release data bus. This will cause a NACK when reading bytes.
+  sdaHi();    // Release data line. This will cause a NACK from master when reading bytes.
   sclHi();
-  ack = digitalRead(SDA_PIN);
+  ack = digitalRead(_sda_pin);
   sclLo();
   return ack;
 }
 
-void writeAck() {  // Used by Master to ACK to slave bewteen multi-byte reads
-  sdaLo();    // Release data bus. This will cause a NACK when reading bytes.
+void SWI2C::writeAck() {  // Used by Master to ACK to slave bewteen multi-byte reads
+  sdaLo();
   sclHi();
   sclLo();
-  sdaHi();
+  sdaHi();  // Release the data line
 }
 
-void writeRegister(int reg_id) {
+void SWI2C::writeRegister(int reg_id) {
   if (reg_id & 0x80) sdaHi();     // bit 7
   else sdaLo();
   sclHi();
@@ -109,100 +124,100 @@ void writeRegister(int reg_id) {
   else sdaLo();
   sclHi();
   sclLo();
-  sdaHi();  // Release the bus for ACK from slave
+  sdaHi();  // Release the data line for ACK from slave
 }
 
-void stopBit() {  // Assume SCK is already LOW (from ack or data write)
+void SWI2C::stopBit() {  // Assume SCK is already LOW (from ack or data write)
   sdaLo();
   sclHi();
   sdaHi();
 }
 
-uint8_t read1Byte() {
+uint8_t SWI2C::read1Byte() {
   uint8_t value = 0;
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x80;
+  if (digitalRead(_sda_pin) == 1) value += 0x80;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x40;
+  if (digitalRead(_sda_pin) == 1) value += 0x40;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x20;
+  if (digitalRead(_sda_pin) == 1) value += 0x20;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x10;
+  if (digitalRead(_sda_pin) == 1) value += 0x10;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x08;
+  if (digitalRead(_sda_pin) == 1) value += 0x08;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x04;
+  if (digitalRead(_sda_pin) == 1) value += 0x04;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x02;
+  if (digitalRead(_sda_pin) == 1) value += 0x02;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x01;
+  if (digitalRead(_sda_pin) == 1) value += 0x01;
   sclLo();
 
   return value;
 }
 
-uint16_t read2Byte() {
+uint16_t SWI2C::read2Byte() {
   uint16_t value = 0;
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x8000;
+  if (digitalRead(_sda_pin) == 1) value += 0x8000;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x4000;
+  if (digitalRead(_sda_pin) == 1) value += 0x4000;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x2000;
+  if (digitalRead(_sda_pin) == 1) value += 0x2000;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x1000;
+  if (digitalRead(_sda_pin) == 1) value += 0x1000;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x0800;
+  if (digitalRead(_sda_pin) == 1) value += 0x0800;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x0400;
+  if (digitalRead(_sda_pin) == 1) value += 0x0400;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x0200;
+  if (digitalRead(_sda_pin) == 1) value += 0x0200;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x0100;
+  if (digitalRead(_sda_pin) == 1) value += 0x0100;
   sclLo();
   writeAck();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x80;
+  if (digitalRead(_sda_pin) == 1) value += 0x80;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x40;
+  if (digitalRead(_sda_pin) == 1) value += 0x40;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x20;
+  if (digitalRead(_sda_pin) == 1) value += 0x20;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x10;
+  if (digitalRead(_sda_pin) == 1) value += 0x10;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x08;
+  if (digitalRead(_sda_pin) == 1) value += 0x08;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x04;
+  if (digitalRead(_sda_pin) == 1) value += 0x04;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x02;
+  if (digitalRead(_sda_pin) == 1) value += 0x02;
   sclLo();
   sclHi();
-  if (digitalRead(SDA_PIN) == 1) value += 0x01;
+  if (digitalRead(_sda_pin) == 1) value += 0x01;
   sclLo();
   return value;
 }
 
-void writeByte(int data) {
+void SWI2C::writeByte(int data) {
   if (data & 0x80) sdaHi();     // bit 7
   else sdaLo();
   sclHi();
@@ -235,12 +250,12 @@ void writeByte(int data) {
   else sdaLo();
   sclHi();
   sclLo();
-  sdaHi();  // Release the bus for ACK from slave
+  sdaHi();  // Release the data line for ACK from slave
 }
 
-int writeToRegister(int deviceID, int regAddress, uint8_t data) {
+int SWI2C::writeToRegister(int regAddress, uint8_t data) {
   startBit();
-  writeAddress(deviceID, 0);
+  writeAddress(0);
   checkAckBit();
   writeRegister(regAddress); // DRCFG register
   checkAckBit();
@@ -250,21 +265,36 @@ int writeToRegister(int deviceID, int regAddress, uint8_t data) {
   return 1;       // Future support for error checking
 }
 
-int readFromRegister(int deviceID, int regAddress, uint8_t* data) {
+int SWI2C::read1bFromRegister(int regAddress, uint8_t* data) {
   startBit();
-  writeAddress(deviceID, 0); // 0 == Write bit
+  writeAddress(0); // 0 == Write bit
   checkAckBit();
   writeRegister(regAddress);
   checkAckBit();
   stopBit();
   startBit();
-  writeAddress(deviceID, 1); // 1 == Read bit
+  writeAddress(1); // 1 == Read bit
   checkAckBit();
-  *data = readByte();
+  *data = read1Byte();
   checkAckBit(); // Master needs to send NACK when done reading data
   stopBit();
   return 1;    // Future support for error checking
 }
 
-// Need a 16-bit (or variable-size) read and write functions
-// Need an ACK-between-read-bytes function (current ACK will signal NACK on reads)
+int SWI2C::read2bFromRegister(int regAddress, uint16_t* data) {
+  startBit();
+  writeAddress(0); // 0 == Write bit
+  checkAckBit();
+  writeRegister(regAddress);
+  checkAckBit();
+  stopBit();
+  startBit();
+  writeAddress(1); // 1 == Read bit
+  checkAckBit();
+  *data = read2Byte();
+  checkAckBit(); // Master needs to send NACK when done reading data
+  stopBit();
+  return 1;    // Future support for error checking
+}
+
+// Need 16-bit (or variable-size) read and write functions
--- src/SWI2C.h
+++ src/SWI2C.h
@@ -1,7 +1,32 @@
 #ifndef SWI2C_H
-
-
-
-
 #define SWI2C_H
+
+#include "Arduino.h"
+
+class SWI2C {
+public:
+  SWI2C(uint8_t sda_pin, uint8_t scl_pin, uint8_t deviceID);
+  void begin();
+  int writeToRegister(int regAddress, uint8_t data);
+  int read1bFromRegister(int regAddress, uint8_t* data);
+  int read2bFromRegister(int regAddress, uint16_t* data);
+  void sclHi();
+  void sclLo();
+  void sdaHi();
+  void sdaLo();
+  void startBit();
+  void writeAddress(int r_w);
+  uint8_t checkAckBit();
+  void writeAck();
+  void writeRegister(int regAddress);
+  void stopBit();
+  uint8_t read1Byte();
+  uint16_t read2Byte();
+  void writeByte(int data);
+private:
+  uint8_t _deviceID;
+  uint8_t _scl_pin;
+  uint8_t _sda_pin;
+};
+
 #endif
